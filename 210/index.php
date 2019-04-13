820ee13d9a0fc2db3ea8c9ad3199cc2e070baaca
Shahadan Bin Saad <shahadansaad@gmail.com>
2019-04-13 10:50:16 +0800

    tambah lagi

--- 210/index.php
+++ 210/index.php
@@ -1,8 +1,10 @@
 <html><head><title>
 <?php
 	echo "Bismillah";
-?>
+
 $mula="Dengan Nama Allah Yang Maha.."; define("AUTHOR", "Shahadan Bin Saad");
+?>
+</title>
 <body>
 <h1><?=$mula?></h1> Quadrillion Terima Kasih
 <div align="right">ikhlas dari <?=AUTHOR?></div>
